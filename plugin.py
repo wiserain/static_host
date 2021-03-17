2ac4ad3400eaec62e3878eb2a176f9a6de345667
wiserain <mail275@gmail.com>
2021-03-17 11:52:14 +0900

    0.0.8 파일도 포함하여 대상 삭제로 이름 변경

--- plugin.py
+++ plugin.py
@@ -42,7 +42,7 @@ def plugin_unload():
 
 plugin_info = {
     "category_name": "tool",
-    "version": "0.0.7",
+    "version": "0.0.8",
     "name": "static_host",
     "home": "https://github.com/wiserain/static_host",
     "more": "https://github.com/wiserain/static_host",
@@ -146,6 +146,8 @@ def ajax(sub):
                 if lpath in drules:
                     if act == 'pur' and os.path.isdir(drules[lpath]['www_root']):
                         shutil.rmtree(drules[lpath]['www_root'])
+                    elif act == 'pur' and os.path.isfile(drules[lpath]['www_root']):
+                        os.remove(drules[lpath]['www_root'])
                     del drules[lpath]
 
             if act:
